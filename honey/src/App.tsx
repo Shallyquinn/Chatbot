8942f3afc8870b9b6b5ba3ee614294b16c923b1a
Omotowa Oluwatobi Shalom <Oluwatobishalom2002@gmail.com>
2025-11-28 14:34:02 +0100

    Fix: Remove undefined handleSaveMessages reference
    - Removed saveMessages prop from Chatbot component
    - This prop was left over from infinite re-render fix
    - Message saving is handled by ActionProvider automatically

--- honey/src/App.tsx
+++ honey/src/App.tsx
@@ -143,7 +143,6 @@ const ChatbotApp: React.FC = () => {
                 config={stableConfig}
                 messageParser={stableMessageParser}
                 actionProvider={stableActionProvider}
-                saveMessages={handleSaveMessages}
               />
             </ChatbotWithDividers>
           </div>
